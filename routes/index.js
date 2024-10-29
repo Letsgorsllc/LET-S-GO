b0d254ccfb9af51cff593d40be0ed6ea5b3fc33b
LET'S GO <DON@letsgors.com>
2024-10-29 08:29:07 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const exampleController = require('../controllers/exampleController');
 
+// Example route
+router.get('/example', exampleController.exampleFunction);
+
+module.exports = router;
